d446d585bf9f369a80a282c953f446c461d44502
midnight-the-rock <kevinrodriguez4851@gmail.com>
2024-05-21 20:43:49 -0600

    inline io functions

--- kernel/arch/i386/io.h
+++ kernel/arch/i386/io.h
@@ -1,21 +1,53 @@
 //// Input/Output utility functions
 
-// mostly being used at kernel/bus/ 
-
 #pragma once
 
 #include <types.h>
 
 namespace io {
 
-  auto in_b(u16 port) ->  u8;
-  auto in_w(u16 port) -> u16;
-  auto in_l(u16 port) -> u32;
+  [[gnu::always_inline]]
+  inline auto in_b(u16 port) -> u8 {
+    u8 data;
+    asm volatile ("inb %1, %0" : "=a"(data) : "d"(port));
 
-  auto out_b(u16 port,  u8 data) -> void;
-  auto out_w(u16 port, u16 data) -> void;
-  auto out_l(u16 port, u32 data) -> void;
+    return data;
+  }
 
-  auto wait() -> void;
+  [[gnu::always_inline]]
+  inline auto in_w(u16 port) -> u16 {
+    u16 data;
+    asm volatile ("inw %1, %0" : "=a"(data) : "d"(port));
+
+    return data;
+  }
+  
+  [[gnu::always_inline]]
+  inline auto in_l(u16 port) -> u32 {
+    u32 data;
+    asm volatile ("inl %1, %0" : "=a"(data) : "d"(port));
+
+    return data;
+  }
+
+  [[gnu::always_inline]]
+  inline auto out_b(u16 port,  u8 data) -> void {
+    asm volatile ("outb %0, %1" :: "a"(data), "d"(port));
+  }
+
+  [[gnu::always_inline]]
+  inline auto out_w(u16 port, u16 data) -> void {
+    asm volatile ("outw %0, %1" :: "a"(data), "d"(port));
+  }
+
+  [[gnu::always_inline]]
+  inline auto out_l(u16 port, u32 data) -> void {
+    asm volatile ("outl %0, %1" :: "a"(data), "d"(port));
+  }
+
+  [[gnu::always_inline]]
+  inline auto wait() -> void {
+    out_b(0x80, 0x00);
+  }
   
 }
